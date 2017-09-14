0ad49582fa4c572bc93c6b147e5ef86c7cda36f8
tongyuehong1 <245503112@qq.com>
2017-09-13 23:01:29 -0700

    验证数组长度对数组内容的影响。

---
+++ length.js
@@ -0,0 +1,15 @@
+/**
+ * 2017-09-14       Tong Yuehong
+ */
+
+var arr = [1, 2, 3];
+console.log(arr.length);
+
+arr.length = 6
+console.log(arr);
+
+arr.length = 2;
+console.log(arr);
+
+arr.length = 4;
+console.log(arr);
