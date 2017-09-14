14890642e224430b61a5ac6573887f2e068f9027
tongyuehong1 <245503112@qq.com>
2017-09-14 15:02:53 +0800

    slice training

---
+++ slice.js
@@ -0,0 +1,28 @@
+/**
+ * 2017-09-14 Tong Yuehong
+ */
+
+var arr = ['a', '1', 'b', '2', 'c', '3', 'd', '4'];
+console.log(`arr.slice(0, 4)=> [ ${arr.slice(0, 4)} ]`);
+
+console.log(`arr.slice(9)=> [ ${arr.slice(9)} ]`);
+
+console.log(`arr.slice(3, 7)=> [ ${arr.slice(3, 7)} ]`);
+
+console.log(`arr.slice(5) => [ ${arr.slice(5)} ]`);
+
+console.log(`arr.slice(-2) =>[ ${arr.slice(-2)} ]`);
+
+console.log(`arr.slice(-5) =>[ ${arr.slice(-5)} ]`);
+
+console.log(`arr.slice(3, 9) =>[ ${arr.slice(3, 9)} ]`);
+
+console.log(`arr.slice(-2, -5) =>[ ${arr.slice(-2, -5)} ]`);
+
+console.log(`arr.slice(-5, -2) =>[ ${arr.slice(-5, -2)} ]`);
+
+console.log(`arr.slice(5, 1) =>[ ${arr.slice(5, 1)} ]`);
+
+console.log(`hello,${arr.slice}`);
+
+console.log(`hello world,${arr.slice(1)}`);
