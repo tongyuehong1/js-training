76ca79a22c74ae663fe2da848248e0fd2c2b8f83
tongyuehong1 <245503112@qq.com>
2017-09-14 15:46:45 +0800

    pop，push等指令练习

---
+++ arr.js
@@ -0,0 +1,21 @@
+/**
+ * 2017-09-14   Tong Yuehong
+ */
+
+var arr =['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
+
+console.log(arr.push('1', '2'));
+console.log(arr);
+
+console.log(arr.pop());
+console.log(arr);
+
+console.log(arr.unshift('3', '4'));
+console.log(arr);
+
+console.log(arr.shift());
+console.log(arr); 
+
+console.log(arr.sort());
+
+console.log(arr.reverse());
