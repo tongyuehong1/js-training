2b1f7144598b801e1a0305607735f7fea2a745ae
tongyuehong1 <245503112@qq.com>
2017-09-16 11:49:26 +0800

    object练习（对象，数组，函数）

--- object.js
+++ object.js
@@ -22,3 +22,49 @@ student.fun();
 student.age();
 
 student.fun();
+console.log(`student ${typeof student} , ${Object.getPrototypeOf(student)}`);
+console.log(JSON.stringify(Object.getOwnPropertyDescriptor(student, 'pass')));
+
+var o = Object.create(null, {
+    foo: {
+        waitable: true,
+        configurable: true,
+        value: 'hello'
+    },
+    bar: {
+        configurable: false,
+        get: function() { return 10; },
+        set: function(value) {
+            console.log('Seting `o.bar` to', value);
+        }
+    }
+});
+console.log(JSON.stringify(Object.getOwnPropertyDescriptor(o, 'foo')));
+
+o.name = 1;
+
+console.log(Object.keys(o));
+console.log(`o ${typeof o}`, Object.getPrototypeOf(o));
+console.log(`o ${typeof o}, ${Object.getPrototypeOf(o)}`);
+
+var arr = ['a', 'b', 'c'];
+arr['name'] = 'tongyuehong';
+arr.age = 1;
+console.log(Object.keys(arr));
+
+console.log(arr['0']);
+
+console.log(arr.name);
+console.log(`arr ${typeof arr}`);
+console.log(Object.getPrototypeOf(arr));
+console.log(Object.getOwnPropertyDescriptor(arr, 'name'));
+
+var a = function (){
+    console.log('abc');
+}
+a.age = 23;
+console.log(Object.keys(a));
+console.log(a.age);
+a();
+console.log(`a ${typeof a} , ${Object.getPrototypeOf(a)}`);
+console.log(Object.getOwnPropertyDescriptor(a, 'age'));
