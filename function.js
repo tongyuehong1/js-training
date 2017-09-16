85c126d5906db78357239c6d038b9cfce81f48c4
tongyuehong1 <245503112@qq.com>
2017-09-16 21:29:02 +0800

    函数包含数组对象函数（使用了get获取）

---
+++ function.js
@@ -0,0 +1,27 @@
+/**
+ * 2017-09-16 Tong Yuehong
+ */
+
+var a = function(){
+};
+a.one = [1, 2];
+a.two = {
+    name: 'abc',
+    sex: 'male',
+}
+a.three = function(){
+    console.log('hello')
+}
+console.log(Object.getOwnPropertyNames(a)); 
+
+
+Object.defineProperty(a, 'len',{
+    configurable: true,
+    get: function() { return this.one.length; },
+});
+
+console.log(a.len);
+
+a.one.length= 10;
+
+console.log(a.len);
