4635a1568077a3de545628f7d20245268349d14e
tongyuehong1 <245503112@qq.com>
2017-09-20 17:37:09 +0800

    不完整

---
+++ animal.js
@@ -0,0 +1,58 @@
+/**
+ * 2017-09-20  Tong Yuehong
+ */
+
+function animal(){
+    
+};
+animal.prototype = {
+    live : true,
+    sex : 'male',
+    eat : function(){
+        console.log(animal.eat);
+    }
+}
+
+function bird(){
+
+};
+bird.prototype = {
+    fly : function(){
+        console.log(bird.fly);
+    }
+}
+bird.prototype.__proto__ = animal.prototype;
+
+function mammal(){
+    
+}; 
+mammal.prototype = {
+    birth : function(){
+        console.log(mammal.birth);
+    }
+}
+mammal.prototype.__proto__ = animal.prototype;
+
+function woodpecker(){
+}
+woodpecker.prototype.__proto__ = bird.prototype;
+var a = new woodpecker();
+
+function sparrow(){
+
+}
+sparrow.prototype.__proto__ = bird.prototype;
+var b = new sparrow();
+
+function dog(){
+
+}
+dog.prototype.__proto__ = mammal.prototype;
+var d = new dog();
+
+
+function cat(){
+
+}
+cat.prototype.__proto__ = mammal.prototype;
+var c = new cat();
