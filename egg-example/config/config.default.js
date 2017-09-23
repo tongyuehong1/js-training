5359ca9b816357495d1889e9ef748b4738cf676a
tongyuehong1 <245503112@qq.com>
2017-09-23 15:24:13 +0800

    egg.js

---
+++ egg-example/config/config.default.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1506131710957_6460';
+
+  // add your config here
+  config.middleware = [];
+
+  return config;
+};
