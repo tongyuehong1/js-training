60c57ef69cb5d0bd50fb3b99f2b742858402a170
tongyuehong1 <245503112@qq.com>
2017-09-24 14:28:14 +0800

    controller下home.js

--- egg-example/app/controller/home.js
+++ egg-example/app/controller/home.js
@@ -1,3 +1,6 @@
+/**
+ * 2017-09-24   Tong Yuehong
+ */
 'use strict';
 
 module.exports = app => {
@@ -7,6 +10,24 @@ module.exports = app => {
     }
     * index1() {
       this.ctx.body = 'hi, 123';
+    }
+    * sina() {
+      const r = yield this.service.home.sina();
+      this.ctx.body = r;
+    }
+    * sohu() {
+      const t = yield this.service.home.sohu();
+      this.ctx.body = t;
+    }
+    * test() {
+      const k = yield this.service.test.test();
+      const r = yield this.service.home.test();
+      this.ctx.body = r + k;
+    }
+    * lodash() {
+      const c = yield this.service.lodash.chunk();
+      const k = yield this.service.lodash.compact();
+      this.ctx.body = `chunk ${c}  compact ${k} `;
     }
   }
   return HomeController;
