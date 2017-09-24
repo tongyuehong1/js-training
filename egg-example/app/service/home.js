e553895e9e205dfa2f0f295dfa59b0ce75bac85d
tongyuehong1 <245503112@qq.com>
2017-09-24 14:28:50 +0800

    service下home.js

--- egg-example/app/service/home.js
+++ egg-example/app/service/home.js
@@ -1,9 +1,21 @@
+/**
+ * 2017-09-24     Tong Yuehong
+ */
 'use strict';
 
 module.exports = app => {
   class Home extends app.Service {
     * find() {
       return 'qq';
+    }
+    * sina() {
+      return yield app.curl('http://www.sina.com.cn');
+    }
+    * sohu() {
+      return yield app.curl('http://www.sohu.com.cn');
+    }
+    * test() {
+      return 'home.test';
     }
   }
   return Home;
