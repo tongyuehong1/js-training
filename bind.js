17ad3ae2650ebeb871887d32f9d66fc76e908a0c
tongyuehong1 <245503112@qq.com>
2017-11-18 11:05:16 +0800

    bind apply call

--- bind.js
+++ bind.js
@@ -20,9 +20,9 @@ function sub(x) {
     this.age = 2;
     return this.age + x
 }
-console.log(sub.apply(a, [10]), a);
+console.log(sub.apply(a, [10]), a); // apply第二个参数是数组，这是和call唯一的区别
 
-var k = sub.bind(a,10);
+var k = sub.bind(a,10); // bind返回了一个新的函数
 console.log(k(), a);
 
 console.log(sub.call(a,10), a)
